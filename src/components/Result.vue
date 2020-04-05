ca5c85422f17169a9e0933e6632edab0fbeb1d1f
Frederick-S <mao_xiaodan@hotmail.com>
2020-04-05 12:04:30 +0000

    feature: render tokens

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,5 +1,6 @@
 <template>
   <section>
+    <b-table :data="data" :columns="columns" :sticky-header="true" :height="400" class="token-list" v-if="data.length > 0"></b-table>
   </section>
 </template>
 
@@ -9,15 +10,63 @@ import axios from '../axios'
 import bus from '../bus'
 import spinner from '../spinner'
 import toast from '../toast'
+import Token from '../token'
+
+interface Data {
+  surface: string;
+
+  partOfSpeech: string;
+
+  baseForm: string;
+
+  reading: string;
+
+  pronunciation: string;
+}
 
 @Component
 export default class Result extends Vue {
+  private columns = [
+    {
+      field: 'surface',
+      label: 'Surface form'
+    },
+    {
+      field: 'partOfSpeech',
+      label: 'Part-of-Speech'
+    },
+    {
+      field: 'baseForm',
+      label: 'Base form'
+    },
+    {
+      field: 'reading',
+      label: 'Reading'
+    },
+    {
+      field: 'pronunciation',
+      label: 'Pronunciation'
+    }
+  ]
+
+  private data: Data[] = []
+
   analyse(text: string) {
     spinner.open()
 
     axios.post('/tokenize', { text })
       .then((data) => {
-        console.log(data)
+        const tokens: Token[] = data.data
+
+        this.data = tokens.map(token => {
+          return {
+            surface: token.surface,
+            partOfSpeech: [token.partOfSpeechLevel1, token.partOfSpeechLevel2, token.partOfSpeechLevel3, token.partOfSpeechLevel4].join('，'),
+            baseForm: token.baseForm,
+            reading: token.reading,
+            pronunciation: token.pronunciation
+          }
+        })
       })
       .catch((error) => [
         toast.danger(error)
---
+++ src/token.ts
@@ -0,0 +1,21 @@
+export default interface Token {
+  surface: string;
+
+  partOfSpeechLevel1: string;
+
+  partOfSpeechLevel2: string;
+
+  partOfSpeechLevel3: string;
+
+  partOfSpeechLevel4: string;
+
+  conjugationType: string;
+
+  conjugationForm: string;
+
+  reading: string;
+
+  baseForm: string;
+
+  pronunciation: string;
+}
